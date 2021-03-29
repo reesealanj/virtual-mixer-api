925dffbbd4f6357dd478f77fb04888946ed337cb
Reese Jones <reesealanj@gwu.edu>
2021-03-28 22:11:14 -0400

    Basic API Implementation

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1069d01d4097d65ad667af84a6c4fdbe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4de14c9-a04c-4a44-b932-8d8038083028'
+};
